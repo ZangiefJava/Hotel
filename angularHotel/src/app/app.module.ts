bf579130b6a90d249f6d728d02f6fc65019c4699
Cariatidee <a.fabi@rocketmail.com>
2022-06-24 17:17:17 +0200

    aggiornameto

--- angularHotel/src/app/app.module.ts
+++ angularHotel/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,12 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'camera', component: CameraComponent }
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
