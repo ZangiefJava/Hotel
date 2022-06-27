d2c81d7fa72fc1220e3e083a50a2f81305358b46
ZangiefJava <dario.defeo91@gmail.com>
2022-06-27 17:30:05 +0200

    senza netbeans

--- ngspringHotel/src/main/java/angular/spring/ngspring/model/RepTipologiaCamera.java
+++
@@ -1,8 +0,0 @@
-package angular.spring.ngspring.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepTipologiaCamera extends JpaRepository<TipologiaCamera, Long>
-{
-
-}
