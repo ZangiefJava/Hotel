9c2e0a9a544a31484db7226d10e07a98e339e272
ZangiefJava <dario.defeo91@gmail.com>
2022-07-11 17:06:06 +0200

    SERVIZIOK2

--- angularHotel/src/app/prenotazione/prenotazione.component.ts
+++ angularHotel/src/app/prenotazione/prenotazione.component.ts
@@ -11,6 +11,7 @@ import { DatiPrenotazioneCameraService } from '../Servizi/dati-prenotazione-came
 import { DatiUtenteService } from '../Servizi/dati-utente.service';
 import { Servizio } from 'src/model/Servizio';
 import { RepositoryServizio } from 'src/model/RepositoryServizio';
+import { Ospite } from 'src/model/Ospite';
 
 
 @Component({
@@ -19,80 +20,98 @@ import { RepositoryServizio } from 'src/model/RepositoryServizio';
   styleUrls: ['./prenotazione.component.css']
 })
 export class PrenotazioneComponent implements OnInit {
-  arrCameraVuoto: Camera[]=[] //non rimuovere!
+  arrCameraVuoto: Camera[] = [] //non rimuovere!
   arrTipologiaCamera: TipologiaCamera[] = []
   arrPrenotazione: PrenotazioneCamera[] = []
-  arrServizio:Servizio[]=[] 
+  arrServizio: Servizio[] = []
   newPrenotazione: PrenotazioneCamera = new PrenotazioneCamera(0, new Date(), new Date(), 500, new Camera(1, new TipologiaCamera(1, "", 0), ""), new Cliente(""))
-  prenotazione:PrenotazioneCamera = new PrenotazioneCamera(0, new Date(), new Date(), 500, new Camera(1, new TipologiaCamera(1, "", 0), ""), new Cliente(""))
-  arrCamera:Camera[]=[]  
-  tipologiaCamera:TipologiaCamera = new TipologiaCamera(0, "", 0, this.arrCameraVuoto) 
-  dataInizio!:Date
-  dataFine!:Date 
-  servizio!:Servizio
-  arrServizio2:Servizio[]=[]
-  arrServizio3:Servizio[] = []
-  
-  
-  
+  prenotazione: PrenotazioneCamera = new PrenotazioneCamera(0, new Date(), new Date(), 500, new Camera(1, new TipologiaCamera(1, "", 0), ""), new Cliente(""))
+  arrCamera: Camera[] = []
+  tipologiaCamera: TipologiaCamera = new TipologiaCamera(0, "", 0, this.arrCameraVuoto)
+  dataInizio!: Date
+  dataFine!: Date
+  servizio!: Servizio
+  arrServizio2: Servizio[] = []
+  arrServizio3: Servizio[] = []
+  prezzoUltimo!: number
+  newOspite: Ospite = new Ospite(0)
+  arrOspite: Ospite[] = []
+
   constructor(
     public repositoryPrenotazione: RepositoryPrenotazione,
-    public repositoryTipologiaCamera:RepositoryTipologiaCamera,
-    public repositoryCamera:RepositoryCamera,    
-    public datiUtenteService:DatiUtenteService,
-    public datiPrenotazioneCameraService:DatiPrenotazioneCameraService,
-    public repositoryServizio:RepositoryServizio
-  ) {}
+    public repositoryTipologiaCamera: RepositoryTipologiaCamera,
+    public repositoryCamera: RepositoryCamera,
+    public datiUtenteService: DatiUtenteService,
+    public datiPrenotazioneCameraService: DatiPrenotazioneCameraService,
+    public repositoryServizio: RepositoryServizio
+  ) { }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.getLista(),
-    this.getListaServizio()
-       
-  } 
-  
-  getLista(){
-    this.repositoryTipologiaCamera.getLista()
-              .subscribe(risp=>{
-                this.arrTipologiaCamera=risp;  
-                console.log("*** "+this.arrTipologiaCamera.length+ " "+this.arrTipologiaCamera)
-  })}
-  
-  
-  
-  getListaCamera(tipologiaCamera:TipologiaCamera) {
-      this.tipologiaCamera= tipologiaCamera
-    }
-      
-  
-    
+      this.getListaServizio()
 
-  cameraXTipologia(id:number){   
-     //let prezzoFinale:number = this.servizio.costo! + this.tipologiaCamera.costoC! 
-     
-     let newPrenotazione1 = new PrenotazioneCamera(0, this.dataInizio, this.dataFine, this.tipologiaCamera.costoC /*prezzoFinale*/, new Camera(id, new TipologiaCamera(1, "", 0), ""), new Cliente(this.datiUtenteService.cliente.user), this.arrServizio2)     
-     this.repositoryPrenotazione.prenota(newPrenotazione1).subscribe(risp => { this.prenotazione = risp; console.log("*** ultimo id "+this.prenotazione.id); })
-     console.log("*** "+this.dataInizio+ " "+this.dataFine + " " + this.datiUtenteService.cliente.user + " prenotazione: " + this.prenotazione)
-    this.newPrenotazione = new PrenotazioneCamera(0, new Date(), new Date(), 100, new Camera(0, new TipologiaCamera(0, "", 0), ""), new Cliente(""), this.arrServizio3)   
-   
   }
-  getListaServizio(){
+
+  getLista() {
+    this.repositoryTipologiaCamera.getLista()
+      .subscribe(risp => {
+        this.arrTipologiaCamera = risp;
+        console.log("*** " + this.arrTipologiaCamera.length + " " + this.arrTipologiaCamera)
+      })
+  }
+
+
+
+  getListaCamera(tipologiaCamera: TipologiaCamera) {
+    this.tipologiaCamera = tipologiaCamera
+  }
+
+
+
+
+  cameraXTipologia(id: number) {
+
+    let prezzoUltimo2 = this.prezzoUltimo! + this.tipologiaCamera.costoC!
+    console.log("*** PREZZO ULTIMO2 " + prezzoUltimo2)
+    let newPrenotazione1 = new PrenotazioneCamera(0, this.dataInizio, this.dataFine, prezzoUltimo2, new Camera(id, new TipologiaCamera(1, "", 0), ""), new Cliente(this.datiUtenteService.cliente.user), this.arrServizio2)
+    this.repositoryPrenotazione.prenota(newPrenotazione1).subscribe(risp => { this.prenotazione = risp; console.log("*** ultimo id " + this.prenotazione.id); })
+    console.log("*** " + this.dataInizio + " " + this.dataFine + " " + this.datiUtenteService.cliente.user + " prenotazione: " + this.prenotazione)
+    this.newPrenotazione = new PrenotazioneCamera(0, new Date(), new Date(), 100, new Camera(0, new TipologiaCamera(0, "", 0), ""), new Cliente(""), this.arrServizio3)
+
+  }
+  getListaServizio() {
     this.repositoryServizio.getListaServizio()
-              .subscribe(risp=>{
-                this.arrServizio=risp;  
-                console.log("*** LISTA SERVIZIO "+this.arrServizio.length+ " "+this.arrServizio+ " "+ this.datiPrenotazioneCameraService.prenotazioneCamera.id)
-  })}
-  selezionaSrv (servizio:Servizio){
-    this.arrServizio2.push(servizio);
-    // console.log("*** COSTO SERVIZIO "+ this.servizio.costo)
-    return this.servizio
+      .subscribe(risp => {
+        this.arrServizio = risp;
+        console.log("*** LISTA SERVIZIO " + this.arrServizio.length + " " + this.arrServizio + " " + this.datiPrenotazioneCameraService.prenotazioneCamera.id)
+      })
   }
-  onChange(servizio:Servizio, event:any){
-    if(!event.target.checked)
-    this.arrServizio2.splice(this.arrServizio2.indexOf(servizio), 1)
-    if(event.target.checked)
-    this.arrServizio2.push(servizio)
-    
+
+
+  onChange(servizio: Servizio, event: any): number {
+    let prezzoFor: number = 0
+    if (!event.target.checked) {
+      this.arrServizio2.splice(this.arrServizio2.indexOf(servizio), 1)
+      for (let i = 0; i < this.arrServizio2.length; i++) {
+        prezzoFor = this.arrServizio2[i].costo! + prezzoFor
+      }
+      console.log("*** PREZZO FOR " + prezzoFor)
+    }
+    if (event.target.checked) {
+      this.arrServizio2.push(servizio)
+      for (let i = 0; i < this.arrServizio2.length; i++) {
+        prezzoFor = this.arrServizio2[i].costo! + prezzoFor
+      }
+      console.log("*** PREZZO FOR " + prezzoFor)
+    }
+    return this.prezzoUltimo = prezzoFor
+  }
+
+  
+  aggiungiOspite(){
+    //this.repositoryOspite.registraOspite(this.newOspite).subscribe(risp=>{this.arrOspite=risp;})
+    console.log("*** "+this.newOspite)
+    this.newOspite= new Ospite(0, "", "", "")
   }
  
 }
